cb2c0db4ebb05eb96bcac334c5719da5915bb93f
tomonori_ueno <tomonori_ueno@sezax.co.jp>
2023-07-05 17:21:51 +0900

    Update code

--- s3/s3_cp_specific_bucket.rb
+++ s3/s3_cp_specific_bucket.rb
@@ -1,17 +1,17 @@
 require 'fileutils'
 
-DIR_LIST = './dir_list.txt'
-BUCKET_NAME = "your-bucket-name/uploads/content/source"
+BUCKET_LIST = './bucket_list.txt'
+S3_BUCKET = "your-bucket-name/uploads/content/source"
 arr = []
 
-File.foreach(DIR_LIST) do |f|
+File.foreach(BUCKET_LIST) do |f|
   f = f.chomp
   arr.push(f)
 end
 
 
 def s3_download(arg)
-  cmd = "aws s3 cp s3://#{BUCKET_NAME}/#{arg} #{arg} --recursive"
+  cmd = "aws s3 cp s3://#{S3_BUCKET}/#{arg} #{arg} --recursive"
   system(cmd)
 end
 
